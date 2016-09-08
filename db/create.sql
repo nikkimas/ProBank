e4fa06bf5cd194df26fbef862335778bc387af62
RBwulf <alex-schuchart@outlook.de>
2016-09-09 00:32:40 +0200

    correction of tables
    putet it in da db

--- db/create.sql
+++ db/create.sql
@@ -1,17 +1,22 @@
+set foreign_key_checks =0;
+
 drop table if exists benutzer;
 create table benutzer(
 name varchar(20),
 nname varchar(20),
-ID int(10)
+ID int not null,
+primary key (ID)
 )engine=InnoDB;
 
 
 drop table if exists konto;
 create table konto(
-kontonummer int(8),
+kontonummer int not null,
 blz int(8),
 saldo int(100),
-kundennummer int(10)
+kundennummer int(10),
+primary key (kontonummer),
+foreign key (kundennummer) references kunde (kundennummer)
 )engine=InnoDB;
 
 
@@ -20,15 +25,20 @@ create table karte(
 kontonummer int(8),
 kundennummer int(10),
 pin int(4), 
-kartennummer int(10),
-status varchar(10)
+kartennummer int not null,
+status varchar(10),
+primary key (kartennummer),
+foreign key (kontonummer) references konto (kontonummer),
+foreign key (kundennummer) references kunde (kundennummer)
 )engine=InnoDB;
 
 
 drop table if exists kunde;
 create table kunde(
-kundennummer int(10),
-ID int(10)
+kundennummer int not null,
+ID int(10),
+primary key (kundennummer),
+foreign key (ID) references benutzer (ID)
 )engine=InnoDB;
 
 
@@ -36,6 +46,7 @@ drop table if exists onlinebkng;
 create table onlinebkng(
 bankingID int(10),
 authcode int(5),
-kundennummer int(10)
+kundennummer int(10),
+foreign key (kundennummer) references kunde (kundennummer)
 )engine=InnoDB;
 
