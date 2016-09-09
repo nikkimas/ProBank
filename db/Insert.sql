12f9334431114c2851813dcb59ef07495a7802fa
RBwulf <alex-schuchart@outlook.de>
2016-09-09 17:18:20 +0200

    i haw chajngejd eweryting

--- db/Insert.sql
+++ db/Insert.sql
@@ -1,12 +1,14 @@
 insert into benutzer
-values ('Chrisopfer','Angerman'),
-		 ('Ginger','Bread'),
-		 ('Gingerman','Bread');
+values ('Chrisopfer','Angerman',NULL),
+		 ('Ginger','Bread',NULL),
+		 ('Gingerman','Bread',NULL);
 		 
 insert into konto
-values (12345678,23456789,225),
-		 (12345454,23456999,25),
-		 (12345677,23457889,2561);
+values (12345678,23456789,225,11),
+		 (12345454,23456999,25,22),
+		 (12345677,23457889,2561,33);
 		 
 insert into karte 
-values (,,1234,),
+values (12345678,11,1234,001002),
+		 (12345454,22,1654,001003),
+		 (12345677,33,1644,001004);
