0863661ae5facd8d6660c5ce5b32a149c09f16c0
MrDj200 <mrdj201@gmail.com>
2016-09-16 07:50:52 +0200

    commit

--- TestFiles/Dj/main.c
+++ TestFiles/Dj/main.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 
-int main(void)
-{
 
+void main(void)
+{
+    int test;
+
+    test = scanfTTY();
+    printf("%i", test);
+    system("pause");
+}
+
+const char * scanfTTY()
+{
+    const char * inp;
+    scanf("%s", inp);
+    fflush(stdin);
+    return inp;
 }
 
 void Clear(void){
@@ -59,5 +72,4 @@ int pinInput(void)
     #endif
 
     return intPin; // Pin an aufrufende funktion zurückgeben
-}
 }
